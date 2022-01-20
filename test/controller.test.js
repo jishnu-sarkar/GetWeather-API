846531fb3a0bd85734e3fd4adeb49bbaa0c95734
jishnu-sarkar <jishnu.sarkar@bitcanny.com>
2022-01-20 13:35:29 +0530

    modified

--- test/controller.test.js
+++ test/controller.test.js
@@ -11,20 +11,22 @@ const weatherController = require("../controllers/weatherController");
 
 describe("weatherController", () => {
   describe("getting currentWeather by IP", () => {
-    let req, res, json, status, fetch;
+    let date, req, res, json, status, fetch;
     beforeEach(() => {
       status = sinon.stub();
       json = sinon.spy();
       res = { json, status };
       status.returns(res);
       fetch = sinon.stub(fetchLib, "fetch");
+      date = new Date().getDate();
+      // date = new Date(result.dt * 1000).toLocaleDateString();
     });
-    // afterEach(() => {
-    //   status.restore();
-    //   json.restore();
-    //   fetch.restore();
-    // });
-    it("should fetch the location using user ip and then fetch the current weather", async () => {
+    afterEach(() => {
+      // status.restore();
+      // json.restore();
+      fetch.restore();
+    });
+    it("Not Getting Weather", async () => {
       req = {
         query: {
           cityName: "Jishnu",
@@ -34,6 +36,22 @@ describe("weatherController", () => {
       const result = await weatherController.currentWeatherCity(req, res);
       expect(status.calledWith(404)).to.be.true;
       expect(json.calledWith({ message: "something went wrong" })).to.be.true;
+    });
+
+    it("Getting Weather", async () => {
+      req = {
+        query: {
+          cityName: "Jishnu",
+        },
+      };
+      fetch.resolves(true);
+      const result = await weatherController.currentWeatherCity(req, res);
+      expect(status.calledWith(202)).to.be.true;
+      expect(
+        json.calledWith({
+          location: "Jishnu",
+        })
+      ).to.be.true;
     });
   });
 });
